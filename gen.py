1057e5c9ce4b1bbef3b0596689d169fceb71dfd3
Nakayama Daiju <42.daiju@gmail.com>
2023-04-29 22:15:28 +0900

    wip

--- gen.py
+++ gen.py
@@ -1,3 +1,5 @@
+from queue import Queue, Empty
+import threading
 import subprocess
 
 
@@ -8,24 +10,60 @@ def battle(a, b):
     return list(map(int, p.stdout.split()))
 
 
+def worker(inq: Queue, outq: Queue):
+    while True:
+        task = inq.get()
+        if task is None:
+            return
+        outq.put([task, battle(*task)])
+
+
+def output_worker(q: Queue, end: threading.Event):
+    while end.is_set():
+        try:
+            (a, b), r = q.get(timeout=1.0)
+        except Empty:
+            continue
+        print(a[0], a[1], b[0], b[1], r[0], r[1], flush=True)
+
+
 def make_one(delta):
-    for a in range(delta, 100, delta):
-        for s in range(delta, 100, delta):
+    for a in range(5, 100, delta):
+        for s in range(1, 100, delta):
             if a + s < 100:
                 yield a, s
 
 
 def main():
+    inq = Queue(maxsize=1024)
+    outq = Queue()
+
+    workers = []
+    for _ in range(6):
+        t = threading.Thread(target=worker, args=(inq, outq))
+        t.start()
+        workers.append(t)
+    end = threading.Event()
+    end.set()
+    ow = threading.Thread(target=output_worker, args=(outq, end))
+    ow.start()
+
     done = set()
-    for a in make_one(5):
-        for b in make_one(5):
+    delta = 1
+    for a in make_one(delta):
+        for b in make_one(delta):
             if a == b:
                 continue
             if (a, b) in done:
                 continue
             done.add((b, a))
-            res = battle(a, b)
-            print(a[0], a[1], b[0], b[1], res[0], res[1])
+            inq.put((a, b))
+    for _ in workers:
+        inq.put(None)
+    for w in workers:
+        w.join()
+    end.clear()
+    ow.join()
 
 
 if __name__ == '__main__':
